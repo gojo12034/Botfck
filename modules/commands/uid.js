66a9ba8f8ee41cedfac47fa2640e90cca92796f3
gojo12034 <gojosatoru123002@gmail.com>
2024-12-17 09:27:43 +0800

    Update uid.js

--- modules/commands/uid.js
+++ modules/commands/uid.js
@@ -1,6 +1,6 @@
 module.exports.config = {
   name: "uid",
-  version: "1.0.0",
+  version: "1.1.0",
   hasPermission: 0,
   credits: "Biru",
   description: "Share a contact or get the user's Facebook UID.",
@@ -10,39 +10,34 @@ module.exports.config = {
 };
 
 module.exports.run = function ({ api, event }) {
-  // Function to share contact and send user ID
-  function shareContact(userID) {
-    const message = ``;
-    api.sendMessage(message, event.threadID, (err) => {
+  // Function to send a message containing the user's Facebook URL and UID
+  function sendUIDMessage(userID) {
+    const facebookURL = `https://www.facebook.com/${userID}`;
+    const message = `🔗 Facebook Profile:\n${facebookURL}\n\n🆔 User ID: ${userID}`;
+    api.sendMessage(message, event.threadID, event.messageID, (err) => {
       if (err) {
         console.error("Failed to send message:", err);
       } else {
-        api.shareContact(userID, userID, event.threadID, (err, data) => {
-          if (err) {
-            console.error("Failed to share contact:", err);
-          } else {
-            console.log("Contact shared successfully:", data);
-          }
-        });
+        console.log(`Successfully sent UID for userID: ${userID}`);
       }
     });
   }
 
-  // Check if there are mentions
+  // Check if there are mentions in the message
   if (Object.keys(event.mentions).length === 0) {
     if (event.messageReply) {
       // If replying to a message, get the senderID of the message being replied to
       const senderID = event.messageReply.senderID;
-      shareContact(senderID);
+      sendUIDMessage(senderID);
     } else {
-      // If no mentions and no message reply, use the sender's ID
+      // If no mentions and no message reply, use the sender's own ID
       const senderID = event.senderID;
-      shareContact(senderID);
+      sendUIDMessage(senderID);
     }
   } else {
     // If there are mentions, loop through each mentioned user
     for (const mentionID in event.mentions) {
-      shareContact(mentionID);
+      sendUIDMessage(mentionID);
     }
   }
 };
