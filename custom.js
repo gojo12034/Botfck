6d32a4c73bce4e010db36bd20871528934920b5c
gojo12034 <gojosatoru123002@gmail.com>
2024-12-20 14:42:34 +0800

    Update custom.js

--- custom.js
+++ custom.js
@@ -15,7 +15,7 @@ module.exports = async ({ api }) => {
   const config = {
     autoRestart: {
       status: true,
-      time: 40,
+      time: 60,
       note: 'To avoid problems, enable periodic bot restarts',
     },
     greetings: [
