d858f6ba5d1e6f8e95619100483212a58fff48ef
gojo12034 <gojosatoru123002@gmail.com>
2025-01-13 17:27:50 +0800

    Update listen.js

--- includes/listen.js
+++ includes/listen.js
@@ -7,76 +7,90 @@ module.exports = function ({ api }) {
   const utils = require("../utils/log.js");
   const { getThemeColors } = utils;
   const { cra, cb, co } = getThemeColors();
+
   //////////////////////////////////////////////////////////////////////
   //========= Push all variable from database to environment =========//
   //////////////////////////////////////////////////////////////////////
 
   (async function () {
-  if (global.config.autoCreateDB) {
     try {
-      const [threads, users] = await Promise.all([
-        Threads.getAll(),
-        Users.getAll(["userID", "name", "data"]),
-      ]);
-      threads.forEach((data) => {
-        const idThread = String(data.threadID);
-        global.data.allThreadID.push(idThread);
-        global.data.threadData.set(idThread, data.data || {});
-        global.data.threadInfo.set(idThread, data.threadInfo || {});
-        if (data.data && data.data.banned) {
-          global.data.threadBanned.set(idThread, {
-            reason: data.data.reason || "",
-            dateAdded: data.data.dateAdded || "",
-          });
-        }
-        if (
-          data.data &&
-          data.data.commandBanned &&
-          data.data.commandBanned.length !== 0
-        ) {
-          global.data.commandBanned.set(idThread, data.data.commandBanned);
-        }
-        if (data.data && data.data.NSFW) {
-          global.data.threadAllowNSFW.push(idThread);
-        }
-      });
-      users.forEach((dataU) => {
-        const idUsers = String(dataU.userID);
-        global.data.allUserID.push(idUsers);
-        if (dataU.name && dataU.name.length !== 0) {
-          global.data.userName.set(idUsers, dataU.name);
-        }
-        if (dataU.data && dataU.data.banned) {
-          global.data.userBanned.set(idUsers, {
-            reason: dataU.data.reason || "",
-            dateAdded: dataU.data.dateAdded || "",
-          });
-        }
-        if (
-          dataU.data &&
-          dataU.data.commandBanned &&
-          dataU.data.commandBanned.length !== 0
-        ) {
-          global.data.commandBanned.set(idUsers, dataU.data.commandBanned);
-        }
-      });
       if (global.config.autoCreateDB) {
+        const [threads, users] = await Promise.all([
+          Threads.getAll(),
+          Users.getAll(["userID", "name", "data"]),
+        ]);
+
+        threads.forEach((data) => {
+          const idThread = String(data.threadID);
+          global.data.allThreadID.push(idThread);
+          global.data.threadData.set(idThread, data.data || {});
+          global.data.threadInfo.set(idThread, data.threadInfo || {});
+          if (data.data && data.data.banned) {
+            global.data.threadBanned.set(idThread, {
+              reason: data.data.reason || "",
+              dateAdded: data.data.dateAdded || "",
+            });
+          }
+          if (
+            data.data &&
+            data.data.commandBanned &&
+            data.data.commandBanned.length !== 0
+          ) {
+            global.data.commandBanned.set(idThread, data.data.commandBanned);
+          }
+          if (data.data && data.data.NSFW) {
+            global.data.threadAllowNSFW.push(idThread);
+          }
+        });
+
+        users.forEach((dataU) => {
+          const idUsers = String(dataU.userID);
+          global.data.allUserID.push(idUsers);
+          if (dataU.name && dataU.name.length !== 0) {
+            global.data.userName.set(idUsers, dataU.name);
+          }
+          if (dataU.data && dataU.data.banned) {
+            global.data.userBanned.set(idUsers, {
+              reason: dataU.data.reason || "",
+              dateAdded: dataU.data.dateAdded || "",
+            });
+          }
+          if (
+            dataU.data &&
+            dataU.data.commandBanned &&
+            dataU.data.commandBanned.length !== 0
+          ) {
+            global.data.commandBanned.set(idUsers, dataU.data.commandBanned);
+          }
+        });
+
         global.loading.log(
-          `Successfully loaded ${cb(`${global.data.allThreadID.length}`)} threads and ${cb(`${global.data.allUserID.length}`)} users`,
-          "LOADED",
+          `Successfully loaded ${cb(
+            `${global.data.allThreadID.length}`
+          )} threads and ${cb(`${global.data.allUserID.length}`)} users`,
+          "LOADED"
+        );
+      } else {
+        global.loading.log(
+          "autoCreateDB is set to false. Skipping data load.",
+          "INFO"
         );
       }
     } catch (error) {
       global.loading.log(
         `Can't load environment variable, error: ${error}`,
-        "error",
+        "error"
       );
     }
   })();
 
   global.loading.log(
-    `${cra(`[ BOT_INFO ]`)} success!\n${co(`[ LOADED ] `)}${cra(`[ NAME ]:`)} ${!global.config.BOTNAME ? "Bot Messenger" : global.config.BOTNAME} \n${co(`[ LOADED ] `)}${cra(`[ BotID ]: `)}${api.getCurrentUserID()}\n${co(`[ LOADED ] `)}${cra(`[ PREFIX ]:`)} ${global.config.PREFIX}`,
-    "LOADED",
+    `${cra(`[ BOT_INFO ]`)} success!\n${co(`[ LOADED ] `)}${cra(`[ NAME ]:`)} ${
+      !global.config.BOTNAME ? "Bot Messenger" : global.config.BOTNAME
+    } \n${co(`[ LOADED ] `)}${cra(`[ BotID ]: `)}${api.getCurrentUserID()}\n${co(
+      `[ LOADED ] `
+    )}${cra(`[ PREFIX ]:`)} ${global.config.PREFIX}`,
+    "LOADED"
   );
 
   const pkg = JSON.parse(fs.readFileSync("package.json", "utf-8"));
@@ -88,8 +102,12 @@ module.exports = function ({ api }) {
 
       if (compareVersions(gitVersion, v) > 0) {
         global.loading.log(
-          `Version ${co(gitVersion)} is available! Consider checking out '${cb("https://github.com/YANDEVA/BotPack")}' for the latest updates.`,
-          "UPDATE",
+          `Version ${co(
+            gitVersion
+          )} is available! Consider checking out '${cb(
+            "https://github.com/YANDEVA/BotPack"
+          )}' for the latest updates.`,
+          "UPDATE"
         );
       } else {
         global.loading.log("Bot is currently up-to-date.", "UPDATE");
@@ -125,6 +143,7 @@ module.exports = function ({ api }) {
       logs();
     }
   });
+
   ///////////////////////////////////////////////
   //========= Require all handle need =========//
   //////////////////////////////////////////////
@@ -134,7 +153,6 @@ module.exports = function ({ api }) {
     Threads,
     Currencies,
   };
-  // Avoid Copy paste! - lianecagara
 
   const handleCommand = require("./handle/handleCommand")(runObj);
   const handleCommandEvent = require("./handle/handleCommandEvent")(runObj);
@@ -153,12 +171,13 @@ module.exports = function ({ api }) {
       return;
     }
   });
+
   //////////////////////////////////////////////////
   //========= Send event to handle need =========//
   /////////////////////////////////////////////////
 
   const Box = require("./liane-box");
-  // abstraction! - lianecagara
+
   return (event) => {
     const box = new Box(api, event, global.config.autoCensor || false);
     const listenObj = {
@@ -188,9 +207,3 @@ module.exports = function ({ api }) {
     }
   };
 };
-
-/** 
-THIZ BOT WAS MADE BY ME(CATALIZCS) AND MY BROTHER SPERMLORD - DO NOT STEAL MY CODE (つ ͡ ° ͜ʖ ͡° )つ ✄ ╰⋃╯
-THIZ FILE WAS MODIFIED BY ME(@YanMaglinte) - DO NOT STEAL MY CREDITS (つ ͡ ° ͜ʖ ͡° )つ ✄ ╰⋃╯
-THIZ FILE WAS MODIFIED BY ANOTHER PERSON(@lianecagara) - box MIT 🫨
-**/
