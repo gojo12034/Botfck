d7ced3a45abada9686cf46c52cda007de254a567
gojo12034 <gojosatoru123002@gmail.com>
2025-01-14 10:26:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ startBotProcess("main.js");
 // startBotProcess("monitor.js");
 //*startBotProcess("telegram/index.js")
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server running on port ${port}`);
 });
+
