26eeff4bf14111b93f054978fc0a3615e0901716
gojo12034 <gojosatoru123002@gmail.com>
2025-01-18 10:54:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -198,7 +198,7 @@ function onBot() {
   bypassRegion: "PNB",
   selfListen: false,
   online: true,
-  userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+  userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:118.0) Gecko/20100101 Firefox/118.0"
   };
   
   login(loginData, async (err, api) => {
